58af3f94eb0f855141675f38644324d4909e64f9
karthik <katikolakarthik@gmail.com>
2025-05-04 21:54:05 +0530

    ntg

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import FormComponent from './components/FormComponent';
-import FocusComponent from './components/FocusComponent';
-import RenderDemoComponent from './components/RenderDemoComponent';
-import NumberTracker from './components/NumberTracker';
-import RenderCounter from './components/RenderCounter';
-import Usestate from './components/UseState';
+// import FormComponent from './components/FormComponent';
+// import FocusComponent from './components/FocusComponent';
+// import RenderDemoComponent from './components/RenderDemoComponent';
+// import NumberTracker from './components/NumberTracker';
+// import RenderCounter from './components/RenderCounter';
+// import Usestate from './components/UseState';
 
-function App() {
-  return (
-    <div>
-      <h1>useRef Hook Examples</h1>
-      <div>
-        <FormComponent />
-        <FocusComponent />
-        <RenderDemoComponent />
-        <NumberTracker />
-        <RenderCounter /> 
+// function App() {
+//   return (
+//     <div>
+//       <h1>useRef Hook Examples</h1>
+//       <div>
+//         <FormComponent />
+//         <FocusComponent />
+//         <RenderDemoComponent />
+//         <NumberTracker />
+//         <RenderCounter /> 
         
-        <Usestate />
-      </div>
-    </div>
-  );
-}
+//         <Usestate />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default App; 
+// export default App; 
